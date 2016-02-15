40f917d42dc34eb5aeb3a7a85b044abf43843749
excelly <excelly@gmail.com>
2016-02-15 12:17:02 -0800

    script to normalize data

---
+++ src/track1/normalize_data.py
@@ -0,0 +1,43 @@
+import os, sys, time
+import cPickle as pkl
+import argparse
+
+import numpy as np
+import scipy as sp
+
+import logging
+logging.basicConfig(format='%(levelname)s %(asctime)s : %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+parser = argparse.ArgumentParser(
+    description="normalize raw text data to numpy format")
+parser.add_argument('--input_file', type=str, 
+                    default='data/track1/rec_log_train.10-1-1')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    args.output_file = args.input_file + '.pkl'
+
+    logger.info('Loading data from %s', args.input_file)
+    data = np.loadtxt(args.input_file, dtype=int, delimiter='\t')
+    logger.info('Loaded data of shape %s', data.shape)
+    
+    raw_user_id = data[:, 0]
+    raw_item_id = data[:, 1]
+
+    uuid, user_id = np.unique(raw_user_id, return_inverse=True)
+    uiid, item_id = np.unique(raw_item_id, return_inverse=True)
+    logger.info('Found %d unique users and %d unique items', 
+                uuid.size, uiid.size)
+
+    data[:, 0] = user_id
+    data[:, 1] = item_id
+
+    logger.info('Saving processed data to %s', args.output_file)
+    with open(args.output_file, 'w') as out:
+        pkl.dump({
+            "uuid": uuid,
+            "uiid": uiid,
+            "rec_log": data
+        }, out, pkl.HIGHEST_PROTOCOL)
